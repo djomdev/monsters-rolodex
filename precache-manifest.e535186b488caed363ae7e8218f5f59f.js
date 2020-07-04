3251bfd3f0f7ad2a2806020a8e91b12407be70bb
Diego Ortega <diego>
2020-07-04 14:37:35 -0500

    Updates

---
+++ precache-manifest.e535186b488caed363ae7e8218f5f59f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50d770cdb08a15daeb41a453cf655ff4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec2bd18599383bc1ec1a",
+    "url": "/monsters-rolodex/static/css/main.c3567907.chunk.css"
+  },
+  {
+    "revision": "a118fcff70fde5bdac91",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7bede9e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2bd18599383bc1ec1a",
+    "url": "/monsters-rolodex/static/js/main.8dd74967.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
